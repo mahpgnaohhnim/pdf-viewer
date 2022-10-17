4bee5c7114bfdd23c0f213cd6d5d5d86bba386ac
Henry Pham <h.pham@pripares.com>
2022-10-18 00:46:25 +0200

    feature: handle enter password for pdf

--- app/src/main/java/de/mahpgnaohhnim/pdfviewer/PdfActivity.kt
+++ app/src/main/java/de/mahpgnaohhnim/pdfviewer/PdfActivity.kt
@@ -1,12 +1,21 @@
 package de.mahpgnaohhnim.pdfviewer
+
+import android.graphics.pdf.PdfRenderer
 import android.os.Bundle
-import android.util.Log
+import android.os.ParcelFileDescriptor
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import com.github.barteksc.pdfviewer.PDFView
 import java.io.File
 
-class PdfActivity: ComponentActivity()  {
+class PdfActivity : ComponentActivity() {
     private lateinit var pdfView: PDFView
+
+    private lateinit var passwordContainer: LinearLayout
+    private lateinit var passwordField: EditText
+    private lateinit var submitBtn: Button
 
     companion object {
         const val PDF_PATH = "PDF_PATH"
@@ -17,17 +26,46 @@ class PdfActivity: ComponentActivity()  {
         setContentView(R.layout.pdf_activity)
         pdfView = findViewById(R.id.pdfView)
 
+        passwordContainer = findViewById(R.id.pdfPasswordContainer)
+        passwordField = findViewById(R.id.pdfPassword)
+        submitBtn = findViewById(R.id.passwordSubmitBtn)
+        submitBtn.setOnClickListener{v -> handleSubmitPsswd()}
+        initPdf()
+    }
+
+    private fun initPdf() {
         val pdfPath: String? = this.intent.extras?.getString(PDF_PATH)
-
-        Log.d("PDFVIEW","PDF VIEWWWWWE")
-
-        if(pdfPath?.isEmpty() == false){
-            val pdfFile = File(pdfPath)
-            if(pdfFile.exists()){
+        val pdfFile = File(pdfPath?: "")
+        if (pdfFile.exists()) {
+            try {
+                PdfRenderer(ParcelFileDescriptor.open(pdfFile, ParcelFileDescriptor.MODE_READ_ONLY))
                 pdfView.fromFile(pdfFile)
                     .defaultPage(0)
                     .load()
+            } catch (e: SecurityException) {
+                pdfView.visibility = PDFView.GONE
+                passwordContainer.visibility = LinearLayout.VISIBLE
             }
+        }
+    }
+
+    private fun handleSubmitPsswd() {
+        val passwd = passwordField.text.toString()
+        if (passwd.isNotBlank()) {
+            openPdf(passwd)
+        }
+    }
+
+    private fun openPdf(passwd: String) {
+        val pdfPath = this.intent.extras?.getString(PDF_PATH)
+        val pdfFile = File(pdfPath ?: "")
+        if (pdfFile.exists()) {
+            passwordContainer.visibility = LinearLayout.GONE
+            pdfView.visibility = PDFView.VISIBLE
+            pdfView.fromFile(pdfFile)
+                .defaultPage(0)
+                .password(passwd)
+                .load()
         }
     }
 }
